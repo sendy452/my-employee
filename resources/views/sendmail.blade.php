b545e5bae8c36d5e711692e31476c0d6b1518154
sendy452 <sendyivenyulian452@gmail.com>
2022-06-15 06:30:32 +0700

    update sendmail

--- resources/views/sendmail.blade.php
+++ resources/views/sendmail.blade.php
@@ -8,6 +8,7 @@
     <p>Pembuatan akun anda berhasil dibuat, dengan email dan password sebagai berikut:</p>
     <p>Email: {{ $details['email'] }}</p>
     <p>Password: {{ $details['password'] }}</p>
+    <p>Mohon untuk segera mengisi data diri pada aplikasi My-Employee, atau dapat mengunduh pada tautan berikut <a href="https://drive.google.com/file/d/1qxKtKwbxB4B0TEUpORpLprwTOq2kAvYa/view?usp=sharing">https://drive.google.com/file/d/1qxKtKwbxB4B0TEUpORpLprwTOq2kAvYa/view?usp=sharing</a></p>
     <p>Terima Kasih</p>
 </body>
 </html>
