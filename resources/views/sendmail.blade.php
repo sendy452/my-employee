fffa22a0342c3c245302d7d51c8c152060b95a53
sendy452 <sendyivenyulian452@gmail.com>
2022-06-27 13:32:31 +0700

    update mail link

--- resources/views/sendmail.blade.php
+++ resources/views/sendmail.blade.php
@@ -8,7 +8,7 @@
     <p>Pembuatan akun anda berhasil dibuat, dengan email dan password sebagai berikut:</p>
     <p>Email: {{ $details['email'] }}</p>
     <p>Password: {{ $details['password'] }}</p>
-    <p>Mohon untuk segera mengisi data diri pada aplikasi My-Employee, atau dapat mengunduh pada tautan berikut <a href="https://drive.google.com/file/d/1qxKtKwbxB4B0TEUpORpLprwTOq2kAvYa/view?usp=sharing">https://drive.google.com/file/d/1qxKtKwbxB4B0TEUpORpLprwTOq2kAvYa/view?usp=sharing</a></p>
+    <p>Mohon untuk segera mengisi data diri pada aplikasi My-Employee, atau dapat mengunduh pada tautan berikut <a href="https://drive.google.com/drive/folders/1zel9_qYRWQRsanjDu52Vx_CYWH1uifVG?usp=sharing">Download Aplikasi</a></p>
     <p>Terima Kasih</p>
 </body>
 </html>
